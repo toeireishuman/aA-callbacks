368df2540224af4fed8377b55965654a88e3c38f
toeireishuman <141298656+toeireishuman@users.noreply.github.com>
2024-10-09 16:40:58 +0800

    Add my solution to 22-first-index

--- try-out-callbacks/problems/22-first-index.js
+++ try-out-callbacks/problems/22-first-index.js
@@ -22,14 +22,17 @@ let result3 = firstIndex(['canine', 'feline', 'tree'], function (s) {
 console.log(result3); // -1
 *******************************************************************************/
 
-let firstIndex = function() {
+let firstIndex = function (array, func) {
+	for (let index = 0; index < array.length; index++) {
+		const element = array[index];
 
+		if (func(element)) {
+			return index;
+		}
+	}
+
+	return -1;
 };
-
-
-
-
-
 
 /*****************DO NOT MODIFY ANYTHING UNDER THIS  LINE**********************/
 module.exports = firstIndex;
